8bb43e7d3fb9aa877e7921a5e910310ea29b54af
Atsushi Nagase <a@ngs.io>
2016-09-12 17:12:39 +0900

    Update script

--- rec_radiko3.sh
+++ rec_radiko3.sh
@@ -2,7 +2,11 @@
 
 set -eu
 
+USERNAME='Radiko Recorder'
 mkdir -p /var/radiko
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"Started recording ${STATION} for ${DURATION_MINUTES} minutes\"}" $SLACK_WEBHOOK_URL
+
 /usr/local/bin/rec_radiko2.sh $STATION $DURATION_MINUTES $RADIKO_LOGIN $RADIKO_PASSWORD /var/radiko
 
 for F in $(ls /var/radiko/*.mp3); do
@@ -10,5 +14,5 @@ for F in $(ls /var/radiko/*.mp3); do
   FNAME=$(basename $F)
   aws s3 cp --acl public-read $F "s3://${S3_BUCKET}/${FNAME}"
   URL="https://${S3_BUCKET}.s3.amazonaws.com/${FNAME}"
-  curl -X POST --data-urlencode "payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"Radiko Recorder\", \"text\": \"Recording ${STATION} for ${DURATION_MINUTES} minutes has been completed\",\"attachments\":[{\"title\":\"${FNAME}\",\"title_link\":\"${URL}\"}]}" $SLACK_WEBHOOK_URL
+  curl -X POST --data-urlencode "payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"Completed recording ${STATION} for ${DURATION_MINUTES} minutes\",\"attachments\":[{\"title\":\"${FNAME}\",\"title_link\":\"${URL}\"}]}" $SLACK_WEBHOOK_URL
 done
